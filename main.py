b1f9c928711dbe4b09bd7d05242e36fd16db76e4
mbardoe <mbardoe@gmail.com>
2022-03-25 08:23:47 -0400

    Move responses out of loop

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 
 def friend():
     prompt="Hello"
+    responses = {"Hello": "How are you?",
+                 "Fine": "Wow, me too.",
+                 "I have to go.": "I will miss you"}
     while True:
         my_text=input(prompt+": ")
-        responses ={"Hello" : "How are you?",
-                    "Fine" : "Wow, me too.",
-                    "I have to go.": "I will miss you"}
+
         if my_text in responses.keys():
             prompt=responses[my_text]
         else:
